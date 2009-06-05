95d3e8106c30d990bf0888b9250413691e545536
Chow Loong Jin <hyperair@gmail.com>
2009-06-06 02:30:19 +0800

    the "Yay we have a blank window" commit
    * .gitignore: Looks like ignoring ./yatta fails. Using unignore rule instead
    * src/Makefile.am: GTKMM_LIBS, not GTKMM_LIBADD. What was I smoking?
    * src/yatta: New files added!

---
+++ src/yatta/ui/main.cc
@@ -0,0 +1,41 @@
+/*      main.cc -- part of the Yatta! Download Manager
+ *      Copyright (C) 2009, Chow Loong Jin <hyperair@gmail.com>
+ *  
+ *      This program is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *  
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *  
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "main.h"
+
+namespace Yatta
+{
+    namespace UI
+    {
+        Main::Main (int argc, char **argv) :
+            Gtk::Main (argc, argv),
+            m_mainwin()
+        {
+        }
+
+        void
+        Main::run ()
+        {
+            m_mainwin.show();
+            Gtk::Main::run ();
+        }
+
+        Main::~Main ()
+        {
+        }
+    };
+};
