20785244470367aa2cf93a811f47eec9051a3abc
Chow Loong Jin <hyperair@gmail.com>
2010-01-06 12:59:44 +0800

    Make test download kill the loop upon completion

--- src/yatta/curl/tests/curl-check.cc
+++ src/yatta/curl/tests/curl-check.cc
@@ -3,6 +3,17 @@
 #include "../manager.h"
 #include "../download.h"
 
+struct bla
+{
+    Glib::RefPtr<Glib::MainLoop> main_loop;
+    bla (Glib::RefPtr<Glib::MainLoop> main_loop) :
+        main_loop (main_loop) {}
+    void operator ()()
+    {
+        g_debug ("Download finished, shutting down loop");
+        main_loop->quit ();
+    }
+};
 int main ()
 {
     Glib::init ();
@@ -10,8 +21,11 @@ int main ()
     Glib::RefPtr<Yatta::Curl::Manager> mgr = Yatta::Curl::Manager::get ();
     mgr->attach ();
 
-    Yatta::Curl::Download dl ("http://tw.releases.ubuntu.com/9.10/ubuntu-9.10-desktop-amd64.iso", "/tmp", "ubuntu.iso");
+    // Yatta::Curl::Download dl ("http://tw.releases.ubuntu.com/9.10/ubuntu-9.10-desktop-amd64.iso", "/tmp", "ubuntu.iso");
     // Yatta::Curl::Download dl ("http://localhost/test.file", "/tmp", "test.file");
+    Yatta::Curl::Download dl ("http://www.ubuntu.com", "/tmp", "testing");
     dl.start ();
-    Glib::MainLoop::create ()->run ();
+    bla func (Glib::MainLoop::create ());
+    dl.connect_signal_finished (sigc::slot<void> (func));
+    func.main_loop->run ();
 }
